6235fb9296a5f6780f8e2ae0e28d0ec76aaaccfc
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2017-05-19 16:20:30 +0200

    Memoize process manager opened window

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ var ProcessManagerWindow = require('./ProcessManagerWindow.js');
 exports.ProcessStatsReporter = ProcessStatsReporter;
 exports.ProcessManagerWindow = ProcessManagerWindow;
 
+let processManagerWindow = null;
 exports.openProcessManager = function() {
-  const window = new ProcessManagerWindow();
-  window.showWhenReady();
+  if (processManagerWindow) {
+    processManagerWindow.focus();
+    return processManagerWindow;
+  }
 
+  processManagerWindow = new ProcessManagerWindow();
+  processManagerWindow.showWhenReady();
+  processManagerWindow.on('closed', () => processManagerWindow = null)
+
+  return processManagerWindow;
 }
